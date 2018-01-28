f33d487ad481a57c765780382a8b6fe19816c410
sitiafiyah <sitiafiyah35@gmail.com>
2018-01-28 19:51:01 +0700

    AddMenuActivityScreenTest

---
+++ app/src/androidTest/java/com/example/android/teatime/MenuActivityScreenTest.java
@@ -0,0 +1,39 @@
+package com.example.android.teatime;
+
+/**
+ * Created by evi on 1/28/2018.
+ */
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.anything;
+
+@RunWith(AndroidJUnit4.class)
+public class MenuActivityScreenTest {
+    public static final String TEA_NAME = "Green Tea";
+
+    @Rule
+    public ActivityTestRule<MenuActivity> mActivityTestRule = new ActivityTestRule<>(MenuActivity.class);
+
+    @Test
+    public void clickGridViewItem_OpensOrderActivity() {
+
+        // Uses {@link Espresso#onData(org.hamcrest.Matcher)} to get a reference to a specific
+        // gridview item and clicks it.
+        onData(anything()).inAdapterView(withId(R.id.tea_grid_view)).atPosition(1).perform(click());
+
+        // Checks that the OrderActivity opens with the correct tea name displayed
+        onView(withId(R.id.tea_name_text_view)).check(matches(withText(TEA_NAME)));
+    }
+}
